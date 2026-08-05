1f9b35be311ea58238d1d0bf4b9f27b5d5128590
chat-du-cheshire <maxlvan@yandex.ru>
2026-08-05 20:52:04 +0300

    07 open api (#6)

--- apps/fundamentals/src/app/coffees/dto/create-coffee.dto.ts
+++ apps/fundamentals/src/app/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({description: 'The name of the coffee'})
   @IsString()
   readonly title!: string;
 
+  @ApiProperty({description: 'The brand of the coffee'})
   @IsString()
   readonly brand!: string;
 
+  @ApiProperty({description: 'The flavors of the coffee', isArray: true})
   @IsString({ each: true })
   readonly flavors!: string[];
 }
--- apps/fundamentals/src/main.ts
+++ apps/fundamentals/src/main.ts
@@ -7,6 +7,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { HttpExceptionFilter } from './app/common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,16 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true }
     })
   )
+
+  const options = new DocumentBuilder()
+    .setTitle('Fundamentals API')
+    .setDescription('The fundamentals API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
